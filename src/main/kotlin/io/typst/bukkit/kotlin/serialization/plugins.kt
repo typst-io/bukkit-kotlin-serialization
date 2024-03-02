77050857cce7dd1732ae43be019971eef4976231
Junhyung Im <entrypointkr@gmail.com>
2024-03-02 20:39:36 +0900

    feat: add plugin extensions

---
+++ src/main/kotlin/io/typst/bukkit/kotlin/serialization/plugins.kt
@@ -0,0 +1,27 @@
+package io.typst.bukkit.kotlin.serialization
+
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+
+val JavaPlugin.configJsonFile: File get() = File(dataFolder, "config.json")
+
+val bukkitPluginJson: Json by lazy {
+    Json {
+        encodeDefaults = true
+        prettyPrint = true
+    }
+}
+
+inline fun <reified A> JavaPlugin.readConfigOrCreate(): A {
+    val configFile = configJsonFile
+    if (configFile.isFile) {
+        return bukkitPluginJson.decodeFromString<A>(configJsonFile.readText())
+    } else {
+        configFile.parentFile.mkdirs()
+        val defaultValue = bukkitPluginJson.decodeFromString<A>("{}")
+        configFile.writeText(bukkitPluginJson.encodeToString<A>(defaultValue))
+        return defaultValue
+    }
+}
